6884c0eed0bdd29561509e10d2811eb80be52b08
juliana <164660231+jpetito@users.noreply.github.com>
2025-06-23 13:38:24 -0300

    Create parciales.sql

---
+++ src/parciales.sql
@@ -0,0 +1,16 @@
+/*Realizar una consulta SQL que retorne para todas las zonas que tengan
+3 (tres) o más depósitos.
+    1) Detalle Zona
+    2) Cantidad de Depósitos x Zona
+    3) Cantidad de Productos distintos compuestos en sus depósitos
+    4) Producto mas vendido en el año 2012 que tenga stock en al menos
+    uno de sus depósitos.
+    5) Mejor encargado perteneciente a esa zona (El que mas vendió en la
+        historia).
+El resultado deberá ser ordenado por monto total vendido del encargado
+descendiente.
+NOTA: No se permite el uso de sub-selects en el FROM ni funciones
+definidas por el usuario para este punto.
+*/
+
+
